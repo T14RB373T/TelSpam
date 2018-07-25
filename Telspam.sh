d6e2292eb559bfd8c21115ca3f217e48c69c251c
T14RB373T <41572760+T14RB373T@users.noreply.github.com>
2018-07-25 12:39:49 +0700

    Add files via upload

---
+++ Telspam.sh
@@ -0,0 +1,38 @@
+#!/system/xbin/bash
+clear
+figlet TelSpam
+echo "\033[31;1m+++++++++++++++++++++++++++++++"
+echo "\033[31;1m Code: T14RB373T Ft Mixangle  +"
+echo "\033[33;1m Team: [Infinity Cyber Team]  +"
+echo "\033[32;1mcontact:tiar.betet25@gmail.com+"
+echo "\033[34;1m+++++++++++++++++++++++++++++++"
+sleep 5
+echo "========================================="
+echo "________________________________________="
+echo "________________________________________="
+echo "________________________________________="
+echo "________________________________________="
+echo "________________________________________="
+echo "========================================="
+sleep 5
+echo "\033[36;1m###############################"
+echo "\033[36;1m=======Special Thanks To======#"
+echo "\033[36;1mF4K3-5Y5T3M|P4TR1CK|Mr.x404   #"
+echo "\033[36;1mMr.Ix | TeguhWidianto |A7SL   #"
+echo "\033[36;1mZET404|Tasikxploiter|HanakoKun#"
+echo "\033[36;1mAll Member Of Infinity Cyber  #"
+echo "\033[36;1m###############################"
+echo "Lanjut?"
+sleep 3
+echo "\033[30;1mKetik 1 Untuk Lanjut"
+sleep 1
+read mrrm
+if
+[ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+echo "\033[31;1m"
+figlet "Spamers"
+php Dx.php
+fi
+
